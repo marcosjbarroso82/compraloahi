76e6c184c696e5af264c96d06a32f1019cb2cbc6
mj1182 <mj1182@gmail.com>
2014-10-04 11:36:23 -0300

    + added first fields to Ad

--- apps/ad/models.py
+++ apps/ad/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class AdQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(publish=True)
+
+class Ad(models.Model):
+    title = models.CharField(max_length=40)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    published = models.BooleanField(default=True)
+    # imagen. fileupload.
+    # tags []
+    # price (moneda)
+    # author. foreingkey.
+
+    objects = AdQuerySet.as_manager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Ad"
+        verbose_name_plural = "Ads"
+        ordering = ["-created"]
