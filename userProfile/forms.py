9e83b698260e5fa3b5056f8a82987b998e5abf7e
mjroson <matiroson@gmail.com>
2014-10-27 19:33:51 -0300

    - Configured ckeditor and facebook login

--- userProfile/forms.py
+++ userProfile/forms.py
@@ -16,4 +16,4 @@ class UserProfileForm(forms.ModelForm):
 Phone_inline_formset = inlineformset_factory(
     UserProfile, Phone, extra=0, min_num=1,  can_delete=True)
 
-Location_inline_formset = inlineformset_factory(UserProfile, UserLocation, extra=0, min_num= 1, can_delete=True)
+Location_inline_formset = inlineformset_factory(UserProfile, UserLocation, extra=0, min_num= 1, max_num=1, can_delete=True)
