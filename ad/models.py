61d472daea1096a1d2d290102cc528aa23681dfa
m <mj1182@gmail.com>
2014-12-03 14:43:01 -0300

    * delete ad images on deletion

--- ad/models.py
+++ ad/models.py
@@ -7,6 +7,12 @@ from taggit.managers import TaggableManager
 
 from taggit.models import GenericTaggedItemBase, TagBase
 
+# Receive the pre_delete signal and delete the file associated with the model instance.
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+
+
+
 
 class AdQuerySet(models.QuerySet):
 
@@ -63,7 +69,10 @@ class Ad(models.Model):
 
 class AdImage(models.Model):
     ad_id = models.ForeignKey(Ad, related_name='images')
-    image = models.ImageField(upload_to='ad', null=False, blank=False)
+    image = models.ImageField(upload_to='ad', null=False, blank=False, )
 
-
+@receiver(pre_delete, sender=AdImage)
+def adImage_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.image.delete(False)
 
