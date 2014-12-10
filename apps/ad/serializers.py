8be1c7a50b7d7579b37e60f66a387c4d007001b9
m <mj1182@gmail.com>
2014-12-10 16:37:15 -0300

    + added thumnail images to ads in the user dashboard

--- apps/ad/serializers.py
+++ apps/ad/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
-from .models import Ad
+from .models import Ad, AdImage
+from sorl.thumbnail import get_thumbnail
+
+
+class AdImageSerializer(serializers.ModelSerializer):
+    thumbnail_90x90 = serializers.SerializerMethodField()
+
+    class Meta:
+        model = AdImage
+        #fields = ('title',)
+
+    def get_thumbnail_90x90(self, obj):
+        return get_thumbnail(obj.image, '90x90', crop='center', quality=99).url
 
 
 class AdSerializer(serializers.ModelSerializer):
-
+    #images = serializers.
+    images = AdImageSerializer(many=True, read_only=True)
     class Meta:
         model = Ad
         #fields = ('title',)
