a86e74c7ed1a5b4d2eab5e1a820c45ea908e04a9
mj1182 <mj1182@gmail.com>
2015-01-21 16:58:08 -0300

    implemented Angular in ad search list

--- static/angular-resources/ad/controllers/ad.controller.js
+++ static/angular-resources/ad/controllers/ad.controller.js
@@ -2,7 +2,6 @@
  * AdCtrl
  * @namespace App.ad.controllers
  */
-var temp;
 
 (function () {
     'use strict';
@@ -11,34 +10,36 @@ var temp;
         .module('App.ad.controllers')
         .controller('AdCtrl', AdCtrl);
 
-    AdCtrl.$inject = ['$scope', 'Ad', 'Snackbar'];
+    AdCtrl.$inject = ['$scope', 'Ad'];
 
     /**
      * @namespace AdCtrl
      */
-    function AdCtrl($scope, Ad, Snackbar) {
-        ////////// MAP //////////////
-        $scope.map = {center: {latitude: -31.4179952, longitude: -64.1890513 }, zoom: 9 };
-        $scope.options = {};
-        $scope.control = {};
+    function AdCtrl($scope, Ad) {
+        $scope.ads = {};
+        $scope.next_page = null;
+        $scope.prev_page = null;
+        $scope.count_page = 0;
+        $scope.current_page = 1;
+        $scope.pages = [];
 
-        $scope.callControl = function() {
-            console.log("callControl");
-            console.log($scope.control);
-        }
-
-        $scope.circle_events = {
-            click: function (marker, eventName, args) {
-                console.log(args.$parent.ad.pk);
-                $('html, body').animate({
-                    scrollTop: $("#ad-anchor-" + args.$parent.ad.pk).offset().top - 70
-                }, 1000);
-            },
-            mouseover: function (marker, eventName, args) {
-                args.$parent.ad.selected = true;
-            },
-            mouseout: function (marker, eventName, args) {
-                args.$parent.ad.selected = false;
+        $scope.map = {
+            center: {latitude: -31.4179952,
+            longitude: -64.1890513 }, zoom: 9,
+            options: {},
+            control: {},
+            circle_events: {
+                click: function (marker, eventName, args) {
+                    $('html, body').animate({
+                        scrollTop: $("#ad-anchor-" + args.$parent.ad.pk).offset().top - 70
+                    }, 1000);
+                },
+                mouseover: function (marker, eventName, args) {
+                    args.$parent.ad.selected = true;
+                },
+                mouseout: function (marker, eventName, args) {
+                    args.$parent.ad.selected = false;
+                }
             }
         };
 
@@ -48,15 +49,6 @@ var temp;
             editable: false, // optional: defaults to false
             visible: true // optional: defaults to true
         }
-        ///////// FIN MAP ////////////
-
-
-        $scope.ads = {};
-        $scope.next_page = null;
-        $scope.prev_page = null;
-        $scope.count_page = 0;
-        $scope.current_page = 1;
-        $scope.pages = [];
 
         getAds();
 
@@ -76,16 +68,6 @@ var temp;
             });
         }
 
-        function setCircleStyle(ad){
-            if (ad.selected) {
-                ad.stroke = {color: '#00FF00', weight: 2, opacity: 1 };
-                ad.fill = {color: '#00AA00', weight: 2, opacity: 1 };
-            } else {
-                ad.stroke = {color: '#FF0000', weight: 2, opacity: 1 };
-                ad.fill = {color: '#AA0000', weight: 2, opacity: 1 };
-            }
-        }
-
         $scope.selectAd = function (ad) {
             ad.selected = true;
             setCircleStyle(ad);
@@ -99,44 +81,31 @@ var temp;
             window.alert("NOT IMLPEMENTED YET! ad" + ad.pk + " should be added to favourites")
         }
 
-        $scope.get_ads_page = function(page){
-            Ad.get({"page":page},function(response) {
-                $scope.current_page = page;
-                $scope.ads = response.results;
-                $scope.next_page = response.next;
-                $scope.prev_page = response.previous;
+        $scope.centerMap = function(position) {
+            $scope.map.control.getGMap().panTo(new google.maps.LatLng(position.latitude, position.longitude));
+        }
 
-                 // set Circle style for each ad
-                $.each($scope.ads,function(index, ad){
-                    setCircleStyle(ad);
-
-                });
-            });
-        };
-
-        $scope.changePage = function(){
-            console.log("CAMBIO DE PAGINA! ");
-        };
-
-        $scope.submitAd= function(text) {
-            var ad = new Ad({text: text});
-            ad.$save(function(){
-                $scope.ads.unshift(ad);
-            })
-        };
-
-        $scope.deleteAd= function(ad) {
-
-            Ad.delete({id:ad.id}, deleteSuccess, deleteError);
-
-            function deleteSuccess(data, headers, status){
-                getAds();
-                //$scope.ads.splice($scope.ads.indexOf(ad),1);
-                Snackbar.show("El aviso fue eliminado con exito!");
+        function setCircleStyle(ad){
+            if (ad.selected) {
+                ad.stroke = {color: '#00FF00', weight: 2, opacity: 1 };
+                ad.fill = {color: '#00AA00', weight: 2, opacity: 1 };
+            } else {
+                ad.stroke = {color: '#FF0000', weight: 2, opacity: 1 };
+                ad.fill = {color: '#AA0000', weight: 2, opacity: 1 };
             }
+        }
 
-            function deleteError(data, headers, status){
-                Snackbar.error("Error al intentar borrar el aviso");
+        $scope.circle_events = {
+            click: function (marker, eventName, args) {
+                $('html, body').animate({
+                    scrollTop: $("#ad-anchor-" + args.$parent.ad.pk).offset().top - 70
+                }, 1000);
+            },
+            mouseover: function (marker, eventName, args) {
+                args.$parent.ad.selected = true;
+            },
+            mouseout: function (marker, eventName, args) {
+                args.$parent.ad.selected = false;
             }
         };
     }
--- static/app.js
+++ static/app.js
@@ -7,14 +7,12 @@ var App = angular.module('App', [
         'ui.bootstrap',
 
         // My lib
-        'App.profile',
         'App.ad',
-        'App.message',
-        'App.userLocation',
         'App.util'
 
     ])
-    .config(function ($interpolateProvider, $httpProvider, $resourceProvider, $stateProvider, $urlRouterProvider) {
+    .config(function ($interpolateProvider, $httpProvider, $resourceProvider, $stateProvider) {
+            console.log("configurando App");
         // Force angular to use square brackets for template tag
         // The alternative is using {% verbatim %}
         $interpolateProvider.startSymbol('{{').endSymbol('}}');
@@ -30,37 +28,5 @@ var App = angular.module('App', [
         // Django expects jQuery like headers
         // $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
-        // Routing
 
-        $urlRouterProvider.otherwise('/');
-
-        $stateProvider
-            .state('default', {
-                url: '/',
-                views: {
-                    'ad-list': {
-                        templateUrl: '/static/angular-resources/ad/templates/ad-list.html',
-                        controller: 'AdCtrl'
-                    },
-                    'ad-map': {
-                        templateUrl: '/static/angular-resources/ad/templates/ad.html',
-                        controller: 'AdCtrl'
-                    }
-                }
-            })
-            // ADS
-            .state('my-ads', {
-                url: '/my-ads',
-                templateUrl: '/static/angular-resources/ad/templates/ad-list.html',
-                controller: 'AdCtrl'
-            })
-
-            // LOCATIONS
-            .state('my-locations', {
-                url: '/my-locations',
-                templateUrl: '/static/angular-resources/user-location/templates/user-locations-list.html',
-                controller: 'UserLocationCtrl'
-            })
-    })
-;
-
+    });
