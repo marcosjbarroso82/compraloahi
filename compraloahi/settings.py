40405427071ba49374deddfc82b73c04fb8b4e97
mjroson <matiroson@gmail.com>
2015-03-04 12:32:03 -0300

    + Add authentication permissions default to all class of API

--- compraloahi/settings.py
+++ compraloahi/settings.py
@@ -214,6 +214,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 
 }
