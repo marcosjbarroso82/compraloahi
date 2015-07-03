9cc93ae0b57985e299747d3a384075d059f738c2
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2015-07-03 15:59:54 -0300

    - Filter fileds in Msg Serializer

--- apps/msg/serializers.py
+++ apps/msg/serializers.py
@@ -9,11 +9,20 @@ class MsgSerializer(serializers.ModelSerializer):
                             'recipient_deleted_at', 'sender', 'moderation_status',
                             'parent', 'thread', 'moderation_reason', 'moderation_date')
 
-class MsgSerializer2(serializers.ModelSerializer):
-    class Meta:
-        model = models.Msg
-        fields = ('pk',)
+    def __init__(self, *args, **kwargs):
+        action = kwargs['context']['view'].action
+        recipient_actions = ['inbox', 'set_read', 'set_trash']
+        sender_actions = ['create', 'sent']
+        not_allowed = ()
 
-        read_only_fields = ('subject', 'body', 'sent_at', 'read_at', 'replied_at', 'sender_archived', 'recipient_archived', 'sender_deleted_at',
-                            'recipient_deleted_at', 'sender', 'moderation_status',
-                            'parent', 'thread', 'moderation_reason', 'moderation_date')
+        super(MsgSerializer, self).__init__(*args, **kwargs)
+
+        # Filter fields depending on whether it's a sender or a recipient
+        if action in recipient_actions:
+            not_allowed = ('sender_archived', 'sender_deleted_at', 'sender',
+                           'moderation_status', 'recipient_deleted_at', 'moderation_reason', 'moderation_date')
+        elif action in sender_actions:
+            not_allowed = ('read_at', 'recipient_archived', 'recipient_deleted_at', 'recipient')
+
+        for field_name in not_allowed:
+            self.fields.pop(field_name)
