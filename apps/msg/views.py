d8500395d02f155c97a200ba8cc443ab8a39b837
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2015-07-03 11:46:37 -0300

    - Implemented Recipient Permission for Msg View Set

--- apps/msg/views.py
+++ apps/msg/views.py
@@ -1,27 +1,36 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
-from .serializers import MsgSerializer, MsgSerializer2
+from .serializers import MsgSerializer
 from . import models
 from datetime import datetime
 from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from rest_framework.permissions import AllowAny
+
+from rest_framework import permissions
 from rest_framework.decorators import permission_classes
 
-class MsgViewSet(viewsets.ModelViewSet):
 
+class IsRecipient(permissions.BasePermission):
+    """
+    Permission to check if a user is a recipient
+    """
+    # def has_permission(self, request, view):
+    #     return True
+
+    def has_object_permission(self, request, view, obj):
+        if obj.recipient == request.user:
+            return True
+        return False
+
+class MsgViewSet(viewsets.ModelViewSet):
+    # TODO: set serializer for Reciepient and Sender
     serializer_class = MsgSerializer
 
-    @detail_route(methods=['patch'])
+    @detail_route(methods=['patch'], permission_classes=[IsRecipient])
     def set_read(self, request, pk=None):
         msg = self.get_object()
-        # self.serializer_class = MsgSerializer2
-
         self.serializer_class = MsgSerializer
-        # serializer = PasswordSerializer(data=request.data)
 
         serializer = self.get_serializer(data=request.data, partial=True)
         if serializer.is_valid():
@@ -36,16 +45,12 @@ class MsgViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return models.Msg.objects.all()
 
-
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
-#@detail_route(methods=['post'], permission_classes=[IsAdminOrIsSelf])
 
-sdfk
-sd
     @list_route(methods=['get'])
     def inbox(self, request):
-        inbox = models.Msg.objects.all()
+        inbox = models.Msg.objects.all().filter(recipient=request.user)
 
         page = self.paginate_queryset(inbox)
         if page is not None:
@@ -53,5 +58,18 @@ sd
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(inbox, many=True)
+        return Response(serializer.data)
+
+    @list_route(methods=['get'])
+    def sent(self, request):
+        # TODO: filter queryset correctly
+        sent = models.Msg.objects.all().filter(sender=request.user)
+
+        page = self.paginate_queryset(sent)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(sent, many=True)
         return Response(serializer.data)
 
