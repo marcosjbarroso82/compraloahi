d55142a71ccf6ac8a194b5d014ca13ee37172370
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2015-07-03 16:36:37 -0300

    - modified serializer

--- apps/msg/serializers.py
+++ apps/msg/serializers.py
@@ -1,6 +1,5 @@
 from . import models
 from rest_framework import serializers
-
 class MsgSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Msg
@@ -10,18 +9,17 @@ class MsgSerializer(serializers.ModelSerializer):
                             'parent', 'thread', 'moderation_reason', 'moderation_date')
 
     def __init__(self, *args, **kwargs):
-        action = kwargs['context']['view'].action
-        recipient_actions = ['inbox', 'set_read', 'set_trash']
-        sender_actions = ['create', 'sent']
-        not_allowed = ()
-
         super(MsgSerializer, self).__init__(*args, **kwargs)
+        user = kwargs['context']['request'].user
 
         # Filter fields depending on whether it's a sender or a recipient
-        if action in recipient_actions:
+        not_allowed = ()
+        if  user == self.instance.recipient:
+            print("Im the recipient")
             not_allowed = ('sender_archived', 'sender_deleted_at', 'sender',
                            'moderation_status', 'recipient_deleted_at', 'moderation_reason', 'moderation_date')
-        elif action in sender_actions:
+        elif user == self.instance.sender:
+            print("Im the sender")
             not_allowed = ('read_at', 'recipient_archived', 'recipient_deleted_at', 'recipient')
 
         for field_name in not_allowed:
