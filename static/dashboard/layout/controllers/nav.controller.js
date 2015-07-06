f5725ea0a75398cfbbeb1a37c4696b67a6d22ea8
mjroson <matiroson@gmail.com>
2015-07-06 19:28:37 -0300

    Change language navigation dropdown menu, fixed logout on dashboard

--- static/dashboard/layout/controllers/nav.controller.js
+++ static/dashboard/layout/controllers/nav.controller.js
@@ -17,9 +17,16 @@
     function NavCtrl(Authentication) {
         var vm = this;
 
+        vm.logout = logout;
+
         vm.msg_unread = Authentication.msg_unread;
         vm.profile = Authentication.profile;
 
+        function logout(){
+            console.log("LOGOUT");
+            Authentication.unauthenticate();
+            window.location = '/users/logout/';
+        }
     }
 
 })();
