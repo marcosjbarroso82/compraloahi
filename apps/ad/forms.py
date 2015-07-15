19788740fea172bf485e14f7cc5a405bbb9df9ea
mjroson <matiroson@gmail.com>
2015-07-15 14:10:45 -0300

    Fixed form not used

--- apps/ad/forms.py
+++ apps/ad/forms.py
@@ -51,64 +51,64 @@ class AdSearchForm(FacetedSearchForm):
         return "AdSearchForm"
 
 
-class TextCheckboxSelectMultiple(forms.widgets.CheckboxSelectMultiple):
-    """
-    Set checked values based on a comma separated list instead of a python list
-    """
-    def render(self, name, value, **kwargs):
-        try:
-            value = list(value)
-
-            value = [cat_id for cat_id in value]
-        except:
-            value = []
-        return super(TextCheckboxSelectMultiple, self).render(name, value, **kwargs)
-
-
-class TextMultiField(forms.MultipleChoiceField):
-    """
-    Work in conjunction with TextCheckboxSelectMultiple to store a
-    comma separated list of multiple choice values in a CharField/TextField
-    """
-    widget = TextCheckboxSelectMultiple
-    def clean(self, value):
-        val = super(TextMultiField, self).clean(value)
-        return val
-
-
-class CreateAdForm(forms.ModelForm):
-    # TODO: ALERT: When init migrate need comment this line
-    categories = TextMultiField(choices=tuple(Category.objects.all().values_list("id", "name")))
-
-    class Meta:
-        model = Ad
-        fields = ('title', 'short_description', 'price', 'body', 'categories')
-        excluded = ('author', 'modified', 'created', 'published', 'pub_date', 'tags' )
-        #widgets = {'body': CKEditorWidget(config_name='awesome_ckeditor'), 'pub_date': forms.TextInput(attrs={'type': 'date', 'class': 'form-control'}) }
-
-
-class AdModifyForm(forms.ModelForm):
-    # TODO: ALERT: When init migrate need comment this line
-    categories = TextMultiField(choices=tuple(Category.objects.all().values_list("id", "name")))
-
-    class Meta:
-        model = Ad
-        fields = ('title','short_description','price', 'body', 'tags', 'categories')
-        excluded = ('author', 'modified', 'pub_date', 'created', 'published')
-        widgets = {'body': CKEditorWidget(config_name='awesome_ckeditor'), 'pub_date': forms.TextInput(attrs={'type': 'date', 'class': 'form-control'}) }
-
-
-from django.forms.extras.widgets import Widget
-
-class AdImageForm(forms.ModelForm):
-    image = forms.ImageField(forms.ClearableFileInput(attrs={'ng-model': 'image'}))
-
-    class Meta:
-        model = AdImage
-        fields = ('image', )
-
-
-AdImage_inline_formset = inlineformset_factory(
-    Ad, AdImage, extra=1, can_delete=True, form=AdImageForm)
-AdLocation_inline_formset = inlineformset_factory(
-    Ad, AdLocation, extra=0, can_delete=False, max_num=1, min_num=1)
+# class TextCheckboxSelectMultiple(forms.widgets.CheckboxSelectMultiple):
+#     """
+#     Set checked values based on a comma separated list instead of a python list
+#     """
+#     def render(self, name, value, **kwargs):
+#         try:
+#             value = list(value)
+#
+#             value = [cat_id for cat_id in value]
+#         except:
+#             value = []
+#         return super(TextCheckboxSelectMultiple, self).render(name, value, **kwargs)
+#
+#
+# class TextMultiField(forms.MultipleChoiceField):
+#     """
+#     Work in conjunction with TextCheckboxSelectMultiple to store a
+#     comma separated list of multiple choice values in a CharField/TextField
+#     """
+#     widget = TextCheckboxSelectMultiple
+#     def clean(self, value):
+#         val = super(TextMultiField, self).clean(value)
+#         return val
+#
+#
+# class CreateAdForm(forms.ModelForm):
+#     # TODO: ALERT: When init migrate need comment this line
+#     categories = TextMultiField(choices=tuple(Category.objects.all().values_list("id", "name")))
+#
+#     class Meta:
+#         model = Ad
+#         fields = ('title', 'short_description', 'price', 'body', 'categories')
+#         excluded = ('author', 'modified', 'created', 'published', 'pub_date', 'tags' )
+#         #widgets = {'body': CKEditorWidget(config_name='awesome_ckeditor'), 'pub_date': forms.TextInput(attrs={'type': 'date', 'class': 'form-control'}) }
+#
+#
+# class AdModifyForm(forms.ModelForm):
+#     # TODO: ALERT: When init migrate need comment this line
+#     categories = TextMultiField(choices=tuple(Category.objects.all().values_list("id", "name")))
+#
+#     class Meta:
+#         model = Ad
+#         fields = ('title','short_description','price', 'body', 'tags', 'categories')
+#         excluded = ('author', 'modified', 'pub_date', 'created', 'published')
+#         widgets = {'body': CKEditorWidget(config_name='awesome_ckeditor'), 'pub_date': forms.TextInput(attrs={'type': 'date', 'class': 'form-control'}) }
+#
+#
+# from django.forms.extras.widgets import Widget
+#
+# class AdImageForm(forms.ModelForm):
+#     image = forms.ImageField(forms.ClearableFileInput(attrs={'ng-model': 'image'}))
+#
+#     class Meta:
+#         model = AdImage
+#         fields = ('image', )
+#
+#
+# AdImage_inline_formset = inlineformset_factory(
+#     Ad, AdImage, extra=1, can_delete=True, form=AdImageForm)
+# AdLocation_inline_formset = inlineformset_factory(
+#     Ad, AdLocation, extra=0, can_delete=False, max_num=1, min_num=1)
