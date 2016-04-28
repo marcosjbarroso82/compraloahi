6bdfdb72db9c61cf228c9897eb7054d99d614294
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2016-04-28 15:01:44 -0300

    advanced in interest groups

--- apps/interest_group/models.py
+++ apps/interest_group/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class InterestGroup(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+    description = models.TextField(max_length=200)
     owner = models.ForeignKey(User)
+    image = models.ImageField(upload_to='groups', null=False, blank=False)
 
     def __str__(self):
         return self.name
--- apps/interest_group/serializers.py
+++ apps/interest_group/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import InterestGroup
+from sorl.thumbnail import get_thumbnail
 
 
 class InterestGroupSerializer(serializers.ModelSerializer):
+    thumbnail_250x160 = serializers.SerializerMethodField()
 
     class Meta:
         model = InterestGroup
+
+    def get_thumbnail_250x160(self, obj):
+        try:
+            return get_thumbnail(obj.image, '250x160', crop='center', quality=99).url
+        except:
+            return ""
 
